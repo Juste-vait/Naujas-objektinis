5a4818e320908b0fdadf3f83d8b6c2414784621b
Juste Vaitkeviciute <juste@Justes-MacBook-Air.local>
2025-05-21 16:22:32 +0300

    add copy n move assignment operators

--- vector.h
+++ vector.h
@@ -50,6 +50,32 @@ private:
         delete[] data_;
     }
 
+    // Kopijavimo priskyrimo operatorius
+    Vector& operator=(const Vector& other) {
+        if (this != &other) {
+            delete[] data_;
+            data_ = new T[other.capacity_];
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            std::copy(other.data_, other.data_ + other.size_, data_);
+        }
+        return *this;
+    }
+
+    // Move priskyrimo operatorius
+    Vector& operator=(Vector&& other) noexcept {
+        if (this != &other) {
+            delete[] data_;
+            data_ = other.data_;
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            other.data_ = nullptr;
+            other.size_ = 0;
+            other.capacity_ = 0;
+        }
+        return *this;
+    }
+
 };
 
 #endif
